3ca2a11585473da8382ba80eee253b0c140d0868
ubuntu-coder <pc.ubuntu.rahimi@gmail.com>
2023-03-25 16:16:50 +0430

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./assets/styles/index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-700">FRABAR Crypto initial Project</h1>
+    </div>
+  );
+}
+
+export default App;
